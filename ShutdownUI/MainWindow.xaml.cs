3a72b9338dceaaaa5625bd5d1a2d34952481a40a
Jester <kj.provenzano@gmail.com>
2015-09-02 19:45:55 -0400

    Added comments to MainWindow.xaml.cs and addedevent handlers to all textboxes pertaining to time.

--- ShutdownUI/MainWindow.xaml.cs
+++ ShutdownUI/MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace ShutdownUI {
 	/// </summary>
 	public partial class MainWindow : Window {
 
+		/// <summary>
+		/// Indicates that the app is shutting down.
+		/// </summary>
 		public bool IsShuttingDown { get; set; }
 
 		public MainWindow() {
@@ -31,21 +34,39 @@ namespace ShutdownUI {
 			this.SetActionButtonForeground();
 		}
 
+		/// <summary>
+		/// Checks if the a given set of characters is allowed.
+		/// </summary>
+		/// <param name="text">The string to evaluate.</param>
+		/// <returns>Returns true if the text does not contain numbers.</returns>
 		private static bool IsTextAllowed(string text) {
 			Regex regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
 			return !regex.IsMatch(text);
 		}
 
+		/// <summary>
+		/// Action button click even handler.
+		/// </summary>
+		/// <param name="sender">The sender object of the evet.</param>
+		/// <param name="e">The even arguments passed in from an action.</param>
 		private void BtnActionClick(object sender , RoutedEventArgs e) {
 			this.IsShuttingDown = !IsShuttingDown;
 			this.SetActionButtonForeground();
 		}
 
+		/// <summary>
+		/// Sets the foreground text and color of the action button based on the state of shutdown.
+		/// </summary>
 		private void SetActionButtonForeground() {
 			this.BtnAction.Foreground = this.IsShuttingDown ? Brushes.PaleVioletRed : Brushes.BlueViolet;
 			this.BtnAction.Content = this.IsShuttingDown ? "Abort!" : "Start";
 		}
 
+		/// <summary>
+		/// Validates the text coming into the textbox and assigns it appropriatly.
+		/// </summary>
+		/// <param name="sender">the sender object</param>
+		/// <param name="e">The even arguments passed in from the action.</param>
 		private void PreviewTextInputRegexValidation(object sender , TextCompositionEventArgs e) {
 			e.Handled = !IsTextAllowed(e.Text);
 		}
