d234bea6b2ca6369189a598961ec65a09df4a4f6
Jester <kj.provenzano@gmail.com>
2015-09-02 19:40:36 -0400

    WPF layout complete. Methods for handling form view have been completed

--- ShutdownUI/MainWindow.xaml.cs
+++ ShutdownUI/MainWindow.xaml.cs
@@ -13,16 +13,41 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DreamLib.Time;
+using System.Text.RegularExpressions;
 
 namespace ShutdownUI {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		public bool IsShuttingDown { get; set; }
+
 		public MainWindow() {
 			InitializeComponent();
-			CbTimeValues.ItemsSource = Enum.GetValues(typeof(TimeUnits)).Cast<TimeUnits>();
-			CbTimeValues.SelectedIndex = 0;
+			CbShutdownOptions.ItemsSource = Enum.GetValues(typeof(ShutdownOptions)).Cast<ShutdownOptions>();
+			CbShutdownOptions.SelectedIndex = 0;
+			this.IsShuttingDown = false;
+			this.SetActionButtonForeground();
+		}
+
+		private static bool IsTextAllowed(string text) {
+			Regex regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+			return !regex.IsMatch(text);
+		}
+
+		private void BtnActionClick(object sender , RoutedEventArgs e) {
+			this.IsShuttingDown = !IsShuttingDown;
+			this.SetActionButtonForeground();
+		}
+
+		private void SetActionButtonForeground() {
+			this.BtnAction.Foreground = this.IsShuttingDown ? Brushes.PaleVioletRed : Brushes.BlueViolet;
+			this.BtnAction.Content = this.IsShuttingDown ? "Abort!" : "Start";
+		}
+
+		private void PreviewTextInputRegexValidation(object sender , TextCompositionEventArgs e) {
+			e.Handled = !IsTextAllowed(e.Text);
 		}
 	}
 }
